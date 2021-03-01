bd394fd24447790824341fe24b0a78b37dd3dc81
igorWebMASTER <igor_1917@hotmail.com>
2021-02-28 22:49:52 -0300

    Update Readme

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -18,52 +18,64 @@ export function Countdown() {
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
   return (
-    <div>
-      <div className={styles.countdownContainer}>
-        <div>
-          <span>{minuteLeft}</span>
-          <span>{minuteRight}</span>
-        </div>
-        <span>:</span>
-        <div>
-          <span>{secondLeft}</span>
-          <span>{secondRight}</span>
-        </div>
-      </div>
-
-      {hasFinished ? (
-        <button className={styles.countDownButton} disabled>
-          <div>Ciclo encerrado </div>
-          <span>
-            <Image
-              src='/icons/check_circle.svg'
-              alt='teste'
-              width={20}
-              height={20}
-            />
-          </span>
+    <>
+      {/* <div className='setTimeout'>
+        Selecione o tempo do ciclo:
+        <br />
+        <button type='button' onClick={setTimeoutNumber(15)}>
+          15 min
         </button>
-      ) : (
-        <>
-          {isActive ? (
-            <button
-              type='button'
-              className={`${styles.countDownButton} ${styles.countDownButtonActive}`}
-              onClick={resetCountdown}
-            >
-              Abandonar ciclo
-            </button>
-          ) : (
-            <button
-              type='button'
-              className={styles.countDownButton}
-              onClick={startCountdown}
-            >
-              Iniciar um ciclo
-            </button>
-          )}
-        </>
-      )}
-    </div>
+        <button type='button' onClick={setTimeoutNumber(25)}>
+          25 min
+        </button>
+      </div> */}
+      <div>
+        <div className={styles.countdownContainer}>
+          <div>
+            <span>{minuteLeft}</span>
+            <span>{minuteRight}</span>
+          </div>
+          <span>:</span>
+          <div>
+            <span>{secondLeft}</span>
+            <span>{secondRight}</span>
+          </div>
+        </div>
+
+        {hasFinished ? (
+          <button className={styles.countDownButton} disabled>
+            <div>Ciclo encerrado </div>
+            <span>
+              <Image
+                src='/icons/check_circle.svg'
+                alt='teste'
+                width={20}
+                height={20}
+              />
+            </span>
+          </button>
+        ) : (
+          <>
+            {isActive ? (
+              <button
+                type='button'
+                className={`${styles.countDownButton} ${styles.countDownButtonActive}`}
+                onClick={resetCountdown}
+              >
+                Abandonar ciclo
+              </button>
+            ) : (
+              <button
+                type='button'
+                className={styles.countDownButton}
+                onClick={startCountdown}
+              >
+                Iniciar um ciclo
+              </button>
+            )}
+          </>
+        )}
+      </div>
+    </>
   );
 }
--- src/components/MenuLeft.tsx
+++ src/components/MenuLeft.tsx
@@ -1,19 +1,22 @@
-// import { useContext } from 'react';
+import { useState } from 'react';
 
 import styles from '../styles/components/MenuLeft.module.css';
 // import { ChallengesContext } from '../contexts/ChallengesContext';
 
 export function MenuLeft() {
   // const { level, closeLevelUpModal } = useContext(ChallengesContext);
-
+  const [buttonMenu, setButtonMenu] = useState();
   return (
     <div className={styles.containerMenu}>
       <div className={styles.imgLogo}>
         <img src='/logo.svg' alt='' />
       </div>
       <div className={styles.buttonNavigation}>
+        <div className={styles.slideButtonMenu}></div>
         <button type='button'>
-          <img src='/home.svg' alt='' />
+          <a href='#' onClick={() => setButtonMenu(true)}>
+            <img src='/home.svg' alt='' />
+          </a>
         </button>
         <button type='button'>
           <img src='/award.svg' alt='' />
